41c71ea04c400daa9afab47d4899abd360835ea0
snackk <santos_matrix@sapo.pt>
2017-02-09 15:14:38 +0000

    Initial commit.

---
+++ Arduino_IRSmartCar/Arduino_IRSmartCar.ino
@@ -0,0 +1,41 @@
+/*
+ Name:		Arduino_IRSmartCar.ino
+ Created:	2/9/2017 3:13:54 PM
+ Author:	snackk
+*/
+
+#include <IRremote.h>
+#include <SmartCarLib.h>
+
+SmartCar smartcar(10, 11, 6, 12, 13, 5);
+
+int RECV_PIN = 9;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() {
+	Serial.begin(9600);
+	irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+
+	if (irrecv.decode(&results)) {
+		Serial.println(results.value, HEX);
+		switch (results.value) {
+		case 0xFD8877:
+			smartcar.handleCar("F\n");
+			break;
+		case 0xFD9867:
+			smartcar.handleCar("B\n");
+			break;
+		case 0xFD28D7:
+			smartcar.handleCar("L\n");
+			break;
+		case 0xFD6897:
+			smartcar.handleCar("R\n");
+			break;
+		}
+		irrecv.resume(); // Receive the next value
+	}
+}
